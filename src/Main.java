9e3cac6cdb15c01a6994c4be4687c18a46c854ec
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-20 20:36:54 -0300

    made some scanner lessons

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Que horas é agora? ");
+        int horas = sc.nextInt();
+
+        if(horas < 12) {
+            System.out.println("Bom dia!");
+        } else if(horas > 12 && horas < 18) {
+            System.out.println("Boa tarde!");
+        } else if(horas > 18 && horas < 24){
+            System.out.println("Boa noite!");
+        } else {
+            System.out.println("Horário inválido");
         }
     }
 }
+
---
+++ src/ScannerTypes.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ScannerTypes {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double total;
+        String firstName;
+        String entireName;
+        int age;
+
+        System.out.println("Type your first name: ");
+        firstName = sc.next(); // first String only input
+
+        sc.nextLine();  //this will solve nextLine problem of consuming the "Enter" key.
+
+        System.out.println("Type your entire name: ");
+        entireName = sc.nextLine(); // String line input
+
+        System.out.println("What is your age? ");
+        age = sc.nextInt(); // Int input
+
+        System.out.println("Type a value: (ex: 12.53) ");
+        total = sc.nextDouble(); // Double input
+
+        System.out.printf("Your name is: %s%n", firstName);
+        System.out.printf("Your entire name is: %s%n", entireName);
+        System.out.printf("Your age is: %d%n", age);
+        System.out.printf("The total is: %.2f", total);
+
+        sc.close();
+    }
+}
