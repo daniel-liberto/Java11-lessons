2aeef7c11e1e75e4688a411c677430e7a1753a16
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-21 15:43:29 -0300

    WhileLoop done

---
+++ src/loop/WhileLoop.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class WhileLoop {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite uma palavra: ");
+    String palavra = sc.nextLine();
+    int counter = 0;
+
+    while (counter <= 10){
+      System.out.printf("Loop: %d, Palavra: %s%n",counter, palavra);
+      counter += 1;
+    }
+    sc.close();
+  }
+}
