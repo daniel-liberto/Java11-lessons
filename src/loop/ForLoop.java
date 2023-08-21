50af4e76c3667f5f1f29dda1a553ff4260f15bca
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-21 16:04:59 -0300

    forloop done

---
+++ src/loop/ForLoop.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class ForLoop {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite uma palavra: ");
+    String word = sc.nextLine();
+    
+    for (int index=0; index < 10; index++){
+      System.out.printf("Index: [%d], Palavra: [%s]%n", index, word);
+    }
+  }
+}
