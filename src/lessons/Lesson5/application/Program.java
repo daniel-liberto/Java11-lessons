8468f6f8a6990d3010d52b68823419d315822e92
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-22 20:07:59 -0300

    lesson5 done

--- src/lessons/Lesson5/application/Program.java
+++ src/lessons/Lesson5/application/Program.java
@@ -1,2 +1,41 @@
-package lessons.Lesson5.application;public class Program {
+package lessons.Lesson5.application;
+
+import lessons.Lesson5.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Product product = new Product();
+    System.out.println("Enter product data: ");
+
+    System.out.print("Name: ");
+    product.name = sc.nextLine();
+
+    System.out.print("Price: ");
+    product.price = sc.nextDouble();
+
+    System.out.print("Quantity: ");
+    product.quantity = sc.nextInt();
+
+//    System.out.printf("Name: %s, Price: %.2f, Quantity: %d", product.name, product.price, product.quantity);
+
+    System.out.println("\nProduct data: " + product);
+
+    System.out.println("\nEnter the number of products to be added in stock: ");
+    product.addProducts(sc.nextInt());
+
+    System.out.println("\nUpdated data: " + product);
+
+    System.out.println("\nEnter the number of products to be removed in stock: ");
+    product.removeProducts(sc.nextInt());
+
+    System.out.println("\nUpdated data: " + product);
+
+    sc.close();
+  }
 }
--- src/lessons/Lesson5/entities/Product.java
+++ src/lessons/Lesson5/entities/Product.java
@@ -1,2 +1,29 @@
-package lessons.Lesson5.application.entities;public class Product {
+package lessons.Lesson5.entities;
+
+public class Product {
+  public String name;
+  public double price;
+  public int quantity;
+
+  public double totalValueInStock() {
+    return price * quantity;
+  }
+
+  public void addProducts(int quantity) {
+    this.quantity += quantity;
+  }
+
+  public void removeProducts(int quantity) {
+    this.quantity -= quantity;
+  }
+
+  public String toString() {
+    return name
+            + ", $"
+            + String.format("%.2f", price)
+            + ", "
+            + quantity
+            + " units, Total: $"
+            + String.format("%.2f", totalValueInStock());
+  }
 }
