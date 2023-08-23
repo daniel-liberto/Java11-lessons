f052df533523c262a5e8363088030174aadeaed9
daniel-liberto <daniel.liberto@hotmail.com>
2023-08-22 21:54:36 -0300

    Lesson7 done about passing values to java classes

--- src/lessons/Lesson7/application/Program.java
+++ src/lessons/Lesson7/application/Program.java
@@ -1,2 +1,31 @@
-package lessons.Lesson7.application;public class Program {
+package lessons.Lesson7.application;
+
+import lessons.Lesson7.entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    Employee person1 = new Employee();
+
+    System.out.println("What is your name? ");
+    person1.Name = sc.nextLine();
+
+    System.out.println("What is your Gross Salary? ");
+    person1.GrossSalary = sc.nextDouble();
+
+    System.out.println("What is the tax you paid for? ");
+    person1.Tax = sc.nextDouble();
+
+    System.out.printf("Employee: %s, $%.2f%n", person1.Name, person1.NetSalary());
+
+    System.out.println("Which percentage to increase salary? ");
+    person1.IncreaseSalary(sc.nextDouble());
+
+    System.out.printf("Updated data: %s, $%.2f", person1.Name, person1.GrossSalary);
+  }
 }
--- src/lessons/Lesson7/entities/Employee.java
+++ src/lessons/Lesson7/entities/Employee.java
@@ -1,2 +1,16 @@
-package lessons.Lesson7.entities;public class Employee {
+package lessons.Lesson7.entities;
+
+public class Employee {
+  public String Name;
+  public double GrossSalary;
+  public double Tax;
+
+  public double NetSalary() {
+      return GrossSalary - Tax;
+  }
+  public void IncreaseSalary(double percentage) {
+      double aux;
+      aux = (percentage / 100) * GrossSalary;
+      GrossSalary = aux + GrossSalary - Tax;
+  }
 }
