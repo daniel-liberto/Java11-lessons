e78af8d0be05e7120d569d0ef23722d294616e2d
daniel-liberto <daniel.liberto@hotmail.com>
2023-09-02 15:07:07 -0300

    need to check id

--- src/lessons/Lesson30/application/Program.java
+++ src/lessons/Lesson30/application/Program.java
@@ -1,6 +1,7 @@
 package lessons.Lesson30.application;
 
-import java.util.ArrayList;
+import lessons.Lesson30.entities.Employee;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -13,35 +14,33 @@ public class Program {
     System.out.println("How many employees will be registered? ");
     int n = sc.nextInt();
 
-    List<String> name = new ArrayList<>();
-    List<Double> salary = new ArrayList<>();
     for (int i = 0; i < n; i++) {
       int id;
       System.out.printf("Employee #%d: %n", i + 1);
-      do{
+
+      List<Employee> employee = null;
+      do {
         System.out.print("id: ");
         id = sc.nextInt();
-      } while (id == 0 && name.get(id) == null);
-
-      sc.nextLine();
-      System.out.print("Name: ");
-      String nameInput = sc.nextLine();
-      name.set(id, nameInput);
-      System.out.println("Salary: ");
-      Double salaryInput = sc.nextDouble();
-      salary.set(id, salaryInput);
+        sc.nextLine();
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Salary: ");
+        Double salary = sc.nextDouble();
+//        employee.add(i, id);
+      } while (employee.get(i).getId() != id);
 
       System.out.println("Enter the employee id that will have salary increase: ");
-      salary.set(id, sc.nextDouble());
+//      salary.set(id, sc.nextDouble());
       System.out.println("Enter the percentage: ");
-      double result = (salary.get(id) / 100) * sc.nextDouble();
-      salary.set(id, result);
+//      double result = (salary.get(id) / 100) * sc.nextDouble();
+//      salary.set(id, result);
 
       System.out.println("List of employees: ");
       System.out.println("[ID]  [NAME]   [SALARY]");
-      for (String user_id: name) {
-        System.out.printf("%d, %s, %.2f", name.indexOf(user_id), user_id, salary.get(name.indexOf(user_id)));
-      }
+//      for (String user_id : name) {
+//        System.out.printf("%d, %s, %.2f", name.indexOf(user_id), user_id, salary.get(name.indexOf(user_id)));
+//      }
     }
   }
 }
