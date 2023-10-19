b156fb3020a1444345b5cd410ac2373ddc196b0c
daniel-liberto <daniel.liberto@hotmail.com>
2023-10-19 14:37:54 -0300

    java inheritance exercise

---
+++ src/lessons2/Lesson08/application/Program.java
@@ -0,0 +1,10 @@
+package lessons2.Lesson08.application;
+
+import java.util.Locale;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+  }
+}
