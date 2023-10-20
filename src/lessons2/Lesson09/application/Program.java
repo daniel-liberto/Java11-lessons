a5dc1190a8ae59d425833273393a239e01859a34
daniel-liberto <daniel.liberto@hotmail.com>
2023-10-20 11:59:52 -0300

    program are now checking if the employee is outsourced and giving the correctly instance

--- src/lessons2/Lesson09/application/Program.java
+++ src/lessons2/Lesson09/application/Program.java
@@ -1,5 +1,10 @@
 package lessons2.Lesson09.application;
 
+import lessons2.Lesson09.entities.Employee;
+import lessons2.Lesson09.entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,6 +13,35 @@ public class Program {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
+    List<Employee> list = new ArrayList<>();
 
+    System.out.println("Enter the number of employees: ");
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      System.out.printf("Employee #%d data: \n", i + 1);
+      System.out.println("Outsourced (y/n)? ");
+      char ch = sc.next().charAt(0);
+      System.out.println("Name: ");
+      sc.nextLine();
+      String name = sc.nextLine();
+      System.out.println("Hours: ");
+      int hours = sc.nextInt();
+      System.out.println("Value per hour: ");
+      double valuePerHour = sc.nextDouble();
+
+      if(ch == 'y' || ch == 'Y'){
+        System.out.println("Additional charge: ");
+        double additionalCharge = sc.nextDouble();
+        Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+        list.add(emp);
+      } else {
+        Employee emp = new Employee(name, hours, valuePerHour);
+        list.add(emp);
+      }
+    }
+
+
+    sc.close();
   }
 }
