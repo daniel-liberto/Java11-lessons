1ee26b62c58a2b68273901bb6db618764162864c
daniel-liberto <daniel.liberto@hotmail.com>
2023-10-24 16:07:50 -0300

    instantiating rectangle and circle shapes

--- src/lessons2/Lesson11/application/Program.java
+++ src/lessons2/Lesson11/application/Program.java
@@ -1,5 +1,12 @@
 package lessons2.Lesson11.application;
 
+import lessons2.Lesson11.entities.Circle;
+import lessons2.Lesson11.entities.Rectangle;
+import lessons2.Lesson11.entities.Shape;
+import lessons2.Lesson11.entities.enums.Color;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,9 +15,32 @@ public class Program {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter the number of shapes: ");
+    System.out.print("Enter the number of shapes: ");
     int n = sc.nextInt();
 
+    for (int i = 0; i < n; i++) {
+      System.out.printf("Shape #%d data: ", i + 1);
+      System.out.print("[R]ectangle or [C]ircle (R/C)? ");
+      char shapeType = sc.next().charAt(0);
+      System.out.print("Color (BLACK/BLUE/RED): ");
+      String colorType = sc.next();
+
+      List<Shape> shapeList = new ArrayList<>();
+
+      if(shapeType == 'R' || shapeType == 'r'){
+        System.out.print("Width: ");
+        Double width = sc.nextDouble();
+        System.out.print("Height: ");
+        Double height = sc.nextDouble();
+        Shape rec = new Rectangle(Color.valueOf(colorType), width, height);
+        shapeList.add(rec);
+      } else if (shapeType == 'C' || shapeType == 'c'){
+        System.out.println("Radius: ");
+        Double radius = sc.nextDouble();
+        Shape rec = new Circle(Color.valueOf(colorType), radius);
+        shapeList.add(rec);
+      }
+    }
 
 
     sc.close();
