6b8186262e38c0a4604d08d55e2cc6d0c5c1d540
daniel-liberto <daniel.liberto@hotmail.com>
2023-10-26 16:09:26 -0300

    stack trace logs error

--- src/lessons2/Lesson13/application/Program.java
+++ src/lessons2/Lesson13/application/Program.java
@@ -7,6 +7,20 @@ import java.util.Scanner;
 public class Program {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
+
+    method1();
+
+    System.out.println("End of program");
+  }
+
+  public static void method1() {
+    System.out.println("***Method 1 START***");
+    method2();
+    System.out.println("***Method 1 END***");
+  }
+
+  public static void method2(){
+    System.out.println("***Method 2 START***");
     Scanner sc = new Scanner(System.in);
     try {
       System.out.println("Type two or more words: ");
@@ -18,12 +32,15 @@ public class Program {
     } catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
       System.out.println("\nThe system gets the following error: ");
       System.out.println("invalid position error");
+      outOfBoundsException.printStackTrace();
+      System.out.println("Type something to continue");
+      sc.next();
     } catch (InputMismatchException inputMismatch) {
       System.out.println("\nThe system gets the following error: ");
       System.out.println("Input error");
     }
 
-
     sc.close();
+    System.out.println("***Method 2 END***");
   }
 }
