a358c5ba1ce9f7af75ccc7d8cee330596785f79b
daniel-liberto <daniel.liberto@hotmail.com>
2023-10-27 19:16:44 -0300

    first commit

---
+++ src/lessons2/Lesson15/application/Program.java
@@ -0,0 +1,10 @@
+package lessons2.Lesson15.application;
+
+import java.util.Locale;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    
+  }
+}
