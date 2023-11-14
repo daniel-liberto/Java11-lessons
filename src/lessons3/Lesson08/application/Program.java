ebfc1c42c472dd67f29bc063eb1f29d3c7e2f7ea
daniel-liberto <daniel.liberto@hotmail.com>
2023-11-14 15:58:19 -0300

    everything going well, need to call Invoice methods

---
+++ src/lessons3/Lesson08/application/Program.java
@@ -0,0 +1,32 @@
+package lessons3.Lesson08.application;
+
+import lessons3.Lesson08.model.entities.CarRental;
+import lessons3.Lesson08.model.entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+    System.out.println("Enter with rent data: ");
+    System.out.print("Car model: ");
+    String carModel = sc.nextLine();
+
+    System.out.println("Start (dd/MM/yyyy hh:mm): ");
+    LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+
+    System.out.println("Finish (dd/MM/yyyy hh:mm): ");
+    LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+    CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+    sc.close();
+  }
+}
