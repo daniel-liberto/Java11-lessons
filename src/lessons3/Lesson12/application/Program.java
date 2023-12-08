748cfa5cb9a17f6988b44575979d870e44e0c675
daniel-liberto <daniel.liberto@hotmail.com>
2023-12-08 14:22:22 -0300

    reading line by line and sorting by alphabetical order

--- src/lessons3/Lesson12/application/Program.java
+++ src/lessons3/Lesson12/application/Program.java
@@ -1,2 +1,32 @@
-package lessons3.Lesson12.application;public class Program {
+package lessons3.Lesson12.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    List<String> list = new ArrayList<>();
+    String path = "C:\\temp\\in.txt";
+
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+      String name = br.readLine();
+      while (name != null){
+        list.add(name);
+        name = br.readLine();
+      }
+      Collections.sort(list);
+      for (String s: list) {
+        System.out.println(s);
+      }
+    }catch (IOException ioException){
+      System.out.println("Error: " + ioException.getMessage());
+    }
+  }
 }
