e16de61bd482847199dc4c3d2fc282516d5c9bb1
daniel-liberto <daniel.liberto@hotmail.com>
2023-12-15 15:09:43 -0300

    first commit of set exercise

--- src/lessons3/Lesson23/application/Program.java
+++ src/lessons3/Lesson23/application/Program.java
@@ -1,2 +1,21 @@
-package lessons3.Lesson23.application;public class Program {
+package lessons3.Lesson23.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter file full path: ");
+    String path = sc.nextLine();
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+    } catch (IOException ioException){
+      System.out.println("Error: " + ioException.getMessage());
+    }
+    sc.close();
+  }
 }
