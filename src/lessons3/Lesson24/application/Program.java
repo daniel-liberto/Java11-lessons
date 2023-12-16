3b8a9c51ad37566c926f19a92a36696de6b0b4f3
daniel-liberto <daniel.liberto@hotmail.com>
2023-12-16 07:14:53 -0300

    set exercise done

--- src/lessons3/Lesson24/application/Program.java
+++ src/lessons3/Lesson24/application/Program.java
@@ -1,30 +1,25 @@
 package lessons3.Lesson24.application;
 
-import java.util.Locale;
-import java.util.Scanner;
+import lessons3.Lesson24.entities.Students;
+
+import java.util.*;
 
 public class Program {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("How many students for course A?");
-    int courseA = sc.nextInt();
-    for (int i = 0; i < courseA; i++) {
-      System.out.printf("Student #%d ID: ", i + 1);
-      int studentID = sc.nextInt();
+    List<String> courses = Arrays.asList("A", "B", "C");
+    Set<Students> studentList = new HashSet<>();
+    for (int i = 0; i < courses.size(); i++) {
+      System.out.printf("How many students for course %s? ", courses.get(i));
+      int course = sc.nextInt();
+      for (int j = 0; j < course; j++) {
+        System.out.printf("Student #%d ID: ", j + 1);
+        int studentID = sc.nextInt();
+        studentList.add(new Students(studentID));
+      }
     }
-    System.out.print("How many students for course B?");
-    int courseB = sc.nextInt();
-    for (int i = 0; i < courseB; i++) {
-      System.out.printf("Student #%d ID: ", i + 1);
-      int studentID = sc.nextInt();
-    }
-    System.out.print("How many students for course C?");
-    int courseC = sc.nextInt();
-    for (int i = 0; i < courseC; i++) {
-      System.out.printf("Student #%d ID: ", i + 1);
-      int studentID = sc.nextInt();
-    }
+    System.out.println("\nTotal students: " + studentList.size());
   }
 }
