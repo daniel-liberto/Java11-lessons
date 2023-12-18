7cce572f747459c2d1d91b64725984aa76979e4d
daniel-liberto <daniel.liberto@hotmail.com>
2023-12-18 16:11:17 -0300

    simple map demo

--- src/lessons3/Lesson25/application/Program.java
+++ src/lessons3/Lesson25/application/Program.java
@@ -1,11 +1,23 @@
 package lessons3.Lesson25.application;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Program {
   public static void main(String[] args) {
 
-    Map<String, String> cookies  = new TreeMap<>();
+    Map<String, String> cookies  = new LinkedHashMap<>();
+
+  cookies.put("username", "maria");
+  cookies.put("email", "maria@gmail.com");
+  cookies.put("phone", "997111122");
+
+  String username = cookies.get("username");
+  cookies.remove("username");
+
+    System.out.println("\nALL COOKIES: ");
+    for (String key: cookies.keySet()) {
+      System.out.println(key + ": " + cookies.get(key));
+    }
   }
 }
