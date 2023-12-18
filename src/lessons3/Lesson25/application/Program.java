849af66242e9de1ce8a624e1e8fd00de58804417
daniel-liberto <daniel.liberto@hotmail.com>
2023-12-18 15:35:13 -0300

    first commit of map lesson

---
+++ src/lessons3/Lesson25/application/Program.java
@@ -0,0 +1,11 @@
+package lessons3.Lesson25.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+  public static void main(String[] args) {
+
+    Map<String, String> cookies  = new TreeMap<>();
+  }
+}
