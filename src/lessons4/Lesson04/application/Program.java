ce74e35d0d4fffb3c33c170e014f7188dd92feeb
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-04 10:58:37 -0300

    lambda way on map stream

--- src/lessons4/Lesson04/application/Program.java
+++ src/lessons4/Lesson04/application/Program.java
@@ -6,6 +6,7 @@ import lessons4.Lesson04.util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -30,5 +31,15 @@ public class Program {
     List<String> namesMethod = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
     System.out.println("\nUppercase method: ");
     namesMethod.forEach(System.out::println);
+
+    Function<Product, String> func = p -> p.getName().toUpperCase();
+    List<String> namesLambda = list.stream().map(func).collect(Collectors.toList());
+    System.out.println("\nUppercase lambda: ");
+    namesLambda.forEach(System.out::println);
+
+    List<String> namesLambdaInline = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+    System.out.println("\nUppercase lambda inline: ");
+    namesLambdaInline.forEach(System.out::println);
+
   }
 }
