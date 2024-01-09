86b3a26785e7ef2dd3b527220b4fb9c1c6e8f5af
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-09 14:25:56 -0300

    stream lesson example

---
+++ src/lessons4/Lesson07/application/Program.java
@@ -0,0 +1,15 @@
+package lessons4.Lesson07.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter full file path: ");
+    String path = sc.next();
+
+  }
+}
---
+++ src/lessons4/Lesson07/entities/Product.java
@@ -0,0 +1,5 @@
+package lessons4.Lesson07.entities;
+
+public class Product {
+  
+}
