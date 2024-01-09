3acd051db4e90b71d60127c1e9bf053f94d2c7e8
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-09 15:54:14 -0300

    stream lesson done

--- src/lessons4/Lesson07/application/Program.java
+++ src/lessons4/Lesson07/application/Program.java
@@ -1,7 +1,12 @@
 package lessons4.Lesson07.application;
 
-import java.util.Locale;
-import java.util.Scanner;
+import lessons4.Lesson07.entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Program {
   public static void main(String[] args) {
@@ -9,7 +14,25 @@ public class Program {
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Enter full file path: ");
-    String path = sc.next();
+    String path = sc.nextLine();
 
+    try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+      List<Product> list = new ArrayList<>();
+
+      String line = br.readLine();
+      while (line != null){
+        String[] fields =line.split(",");
+        list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+        line = br.readLine();
+      }
+      double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
+      System.out.println("Average price: " + String.format("%.2f", avg));
+      Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+      List<String> names = list.stream().filter(x -> x.getPrice() < avg).map(x -> x.getName()).sorted(comp.reversed()).collect(Collectors.toList());
+      names.forEach(System.out::println);
+    } catch(IOException ioException){
+      System.out.println("Error: " + ioException.getMessage());
+    }
   }
 }
