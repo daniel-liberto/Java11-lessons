1ec5670b59eeff99407650fafde8f04e1a38e1b5
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-16 10:47:22 -0300

    update lesson in jdbc

---
+++ src/lessonsDB3/application/Program.java
@@ -0,0 +1,9 @@
+package lessonsDB3.application;
+
+public class Program {
+  public static void main(String[] args) {
+
+    
+
+  }
+}
---
+++ src/lessonsDB3/exceptions/DbException.java
@@ -0,0 +1,7 @@
+package lessonsDB3.exceptions;
+
+public class DbException extends RuntimeException{
+  public DbException(String msg){
+    super(msg);
+  }
+}
