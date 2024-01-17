aacd68d2f17cde41092a5a263eb7349bfd872984
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-17 16:49:14 -0300

    Seller entity class

--- src/lessonsDatabase/lessonsDB6/application/Program.java
+++ src/lessonsDatabase/lessonsDB6/application/Program.java
@@ -1,11 +1,23 @@
 package lessonsDatabase.lessonsDB6.application;
 
 import lessonsDatabase.lessonsDB6.model.entities.Department;
+import lessonsDatabase.lessonsDB6.model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
   public static void main(String[] args) {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     Department obj = new Department(1, "Books");
-    System.out.println(obj);
+    try {
+      Seller seller = new Seller(21, "Bob", "bob@gmail.com", sdf.parse("24/12/2001"), 3000.0, obj);
+      System.out.println(seller);
+    } catch (ParseException parseException){
+      System.out.println(parseException.getMessage());
+    }
+
+
   }
 }
