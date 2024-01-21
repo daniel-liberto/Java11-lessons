e2ff04456faac398fe759c7deb1f599dadabad0c
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-21 10:53:09 -0300

    minor change

--- src/lessonsDatabase/lessonsDB6/application/Program2.java
+++ src/lessonsDatabase/lessonsDB6/application/Program2.java
@@ -2,7 +2,6 @@ package lessonsDatabase.lessonsDB6.application;
 
 import lessonsDatabase.lessonsDB6.model.dao.DaoFactory;
 import lessonsDatabase.lessonsDB6.model.dao.DepartmentDao;
-import lessonsDatabase.lessonsDB6.model.entities.Department;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -13,21 +12,21 @@ public class Program2 {
     Scanner sc = new Scanner(System.in);
     DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-    System.out.println("=== TEST 1: Insert department ===");
-    System.out.print("Type a department name: ");
-    String depName = sc.nextLine();
-    Department dep = new Department(null, depName);
-    departmentDao.insert(dep);
-    System.out.println("New department created! new id: " + dep.getId());
+//    System.out.println("=== TEST 1: Insert department ===");
+//    System.out.print("Type a department name: ");
+//    String depName = sc.nextLine();
+//    Department dep = new Department(null, depName);
+//    departmentDao.insert(dep);
+//    System.out.println("New department created! new id: " + dep.getId());
 
-    System.out.println("=== TEST 2: Update department ===");
-    System.out.print("Type a new name: ");
-    String depUpdateName = sc.nextLine();
-    System.out.println("Type id number from department " + depUpdateName);
-    int depUpdateId = sc.nextInt();
-    Department depUpdate = new Department(depUpdateId, depUpdateName);
-    departmentDao.update(depUpdate);
-    System.out.println("Update completed!");
+//    System.out.println("=== TEST 2: Update department ===");
+//    System.out.print("Type a new name: ");
+//    String depUpdateName = sc.nextLine();
+//    System.out.println("Type id number from department " + depUpdateName);
+//    int depUpdateId = sc.nextInt();
+//    Department depUpdate = new Department(depUpdateId, depUpdateName);
+//    departmentDao.update(depUpdate);
+//    System.out.println("Update completed!");
 
     sc.close();
   }
